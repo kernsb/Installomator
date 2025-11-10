7fef6628e7c47aa46bb8b284497b734817fc2806
Kerns, Brandon W <bkerns@purdue.edu>
2025-11-10 15:01:18 -0500

    Snagit URL Fixes

--- fragments/labels/snagit2019.sh
+++ fragments/labels/snagit2019.sh
@@ -1,7 +1,7 @@
 snagit2019)
     name="Snagit 2019"
     type="dmg"
-    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2019.0.0 Sparkle/2.8.0' 'https://www.techsmith.com/redirect.asp?target=sufeedurl&product=snagitmac&ver=2019.0.0&lang=enu&os=mac')
+    sparkleData=$(curl -fsL -H 'User-Agent: Snagit/2019.0.0' 'https://www.techsmith.com/redirect.asp?target=sufeedurl&product=snagitmac&ver=2019.0.0&lang=enu&os=mac')
     appNewVersion=$(
         echo "$sparkleData" | \
         xmllint -xpath 'string(//*[local-name()="item"][1]/title/text())' - | \
