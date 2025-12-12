611db1eda020688658a5540960ed87fc6940266a
Kerns, Brandon W <bkerns@purdue.edu>
2025-12-12 09:34:51 -0500

    Outset fix

--- fragments/labels/outset.sh
+++ fragments/labels/outset.sh
@@ -4,6 +4,7 @@ outset)
     packageID="io.macadmins.Outset"
     downloadURL=$(downloadURLFromGit "macadmins" "outset")
     appNewVersion=$(versionFromGit "macadmins" "outset")
+    versionKey="CFBundleShortVersionString"
     expectedTeamID="T4SK8ZXCXG"
     blockingProcesses=( NONE )
     ;;
